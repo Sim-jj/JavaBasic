b503ed1c8969850b1e913b4020af9916ec214a9d
Sim-jj <vnzkthsu@gmail.com>
2020-11-17 15:01:53 +0900

    내가 만든 성적 v2b

--- src/simjj/SungJukV2.java
+++ src/simjj/SungJukV2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * 파일명 : sungJukV2
  * 작성일 : 2020.11.17
  *
- * 프로그램 설명: 성적처리프로그램 v2
+ * 프로그램 설명: 성적처리프로그램 v2b
  * 이름,국어,영어,수학점수를 키보드로 입력받아
  * 총점,평균,학점을 계산하고 결과출력
  *
@@ -16,7 +16,8 @@ import java.util.Scanner;
  *  * 100~90,89~80,79~70,69~60,59~50
  * name,kor,eng,mat,sum,mean(평균),grd(학점)
  *
- *
+ *단, 성적처리 결과 출력시 출력문을 하나만 사용하기로 함
+ * 그리고, Math.round(값)를 이용해서 평균은 소수점 둘째자리까지 출력
  */
 public class SungJukV2 {
     public static void main(String[] args) {
@@ -61,17 +62,17 @@ public class SungJukV2 {
             default: grd = '가';
         }
 
-        //String.format(형식지정자, 변수들)
+        /* //String.format(형식지정자, 변수들)
         mean = Double.parseDouble(String.format("%.1f",mean));//String포맷은 문자열이기 때문에 실수형으로 변형시켜줘야 됨으로
         //Double.parseDouble를 이용
+        */
+
+        mean = Math.round(mean*100)/100.0;
 
         //결과 출력
-        System.out.println("이름 : " + name);
-        System.out.println("국어 : " + kor);
-        System.out.println("영어 : " + eng);
-        System.out.println("수학 : " + mat);
+        System.out.printf("이름 : %s \n국어 : %d \n영어 : %d \n수학 : %d \n",name,kor,eng,mat);
         System.out.println("--------------");
-        System.out.println("총점 : " + sum);
+        System.out.printf("총점 : %d \n평균 : %f \n학점 : %c \n",sum,mean,grd);
         //System.out.format("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
         //System.out.printf("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
         System.out.println("평균 : " + mean);
