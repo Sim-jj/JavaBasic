7342fdfc770f1a0e8b28e2236300f46463c5ec75
Sim-jj <vnzkthsu@gmail.com>
2020-11-17 12:58:15 +0900

    선생님이 만든 성적 v2

--- src/simjj/SungJukV2.java
+++ src/simjj/SungJukV2.java
@@ -29,38 +29,41 @@ public class SungJukV2 {
         double mean = 0.0;
         char grd = '가';
 
-        //값을 입력받음
-        System.out.println("이름을 입력하세요");
-        Scanner scanner = new Scanner(System.in);
-        name = scanner.nextLine();
+        /*처리
+        성적데이터를 키보드로 입력 받기위해
+        Scanner 클래스 초기화*/
 
-        System.out.println("국어 점수를 입력하세요");
-        kor = scanner.nextInt();
+        System.out.print("이름을 입력하세요 : "); //print->는 줄바꿈을 안한다
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine(); //nextLine();-> 문자열 입력 받을때
 
-        System.out.println("영어 점수를 입력하세요");
-        eng = scanner.nextInt();
+        System.out.print("국어 점수를 입력하세요 : ");
+        kor = sc.nextInt(); //nextInt();-> 정수형 입력 받을때 (참고 실수형은 nextDouble();)
 
-        System.out.println("수학 점수를 입력하세요");
-        mat = scanner.nextInt();
+        System.out.print("영어 점수를 입력하세요 : ");
+        eng = sc.nextInt();
+
+        System.out.print("수학 점수를 입력하세요 : ");
+        mat = sc.nextInt();
 
         //처리
         sum =  kor + eng + mat;
-        mean = (double)sum / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 더블형이도 인트형으로 저장된다
+        mean = (double)sum / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 double형이도 int형으로 저장된다
 
-        //switch문
-        switch((int)mean/10){ //평균을 10으로 나누었을때 10,9,8...으로 떨어지기때문에,mean이 실수형이기 때문에 int으로 캐스트하여 계산을 해준다
-            case 10 : grd = '수';
-            break;
-            case 9 : grd = '수';
-            break;
-            case 8 : grd = '우';
-            break;
-            case 7 : grd = '미';
-            break;
-            case 6 : grd = '양';
-            break;
+        //학점 계산은 switch문으로 처리
+        switch((int)(mean/10)) { //평균을 10으로 나누었을때 10,9,8...으로 떨어지기때문에(int형은 소수점이 생길경우 다 버린다/예를들어 9.8일경우 9)
+            // ,mean이 실수형이기 때문에 int으로 캐스트하여 계산을 해준다
+            case 10 :  //간단한 경우 는 왠만하면 한줄로 코딩하는게 좋다 10,9는 같은 처리를 하기 때문에 9: 부분에만 처리값을 넣어줌
+            case 9 : grd = '수'; break;
+            case 8 : grd = '우'; break;
+            case 7 : grd = '미'; break;
+            case 6 : grd = '양'; break;
             default: grd = '가';
         }
+
+        //String.format(형식지정자, 변수들)
+        mean = Double.parseDouble(String.format("%.1f",mean));//String포맷은 문자열이기 때문에 실수형으로 변형시켜줘야 됨으로
+        //Double.parseDouble를 이용
 
         //결과 출력
         System.out.println("이름 : " + name);
@@ -69,7 +72,9 @@ public class SungJukV2 {
         System.out.println("수학 : " + mat);
         System.out.println("--------------");
         System.out.println("총점 : " + sum);
-        System.out.format("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
+        //System.out.format("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
+        //System.out.printf("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
+        System.out.println("평균 : " + mean);
         System.out.println("학점 : " + grd);
     }
 }
