d6ccc172bd5053d3bcad0c879f736eabf4e4030c
Sim-jj <vnzkthsu@gmail.com>
2020-11-17 11:46:56 +0900

    선생님이 작성한 v1

--- src/simjj/SungJukV1.java
+++ src/simjj/SungJukV1.java
@@ -12,48 +12,38 @@ import java.util.Scanner;
  * name,kor,eng,mat,sum,mean(평균),grd(학점)
  * 학점기준은 수우미양가로 함
  * 만점은 100점으로, 10점단위로 학점을 평가
+ * 100~90,89~80,79~70,69~60,59~50
+ *
+ * 삼항연산자 : (조건식)? 참일때 처리 : 거짓일때 처리
  */
 public class SungJukV1 {
     public static void main(String[] args) {
+        // 변수선언
+        String name = "재선";
+        int kor = 99;
+        int eng = 98;
+        int mat = 99;
+        int sum = 0;
+        double mean = 0.0;
+        char grd = '가';
 
-        String name = "심재선";
-        String grd1 = "수", grd2 = "우", grd3 = "미", grd4 = "양", grd5 = "가";
-        int kor = 98, eng = 99, mat = 99, sum;
-        double mean;
+        //처리
+        sum =  kor + eng + mat;
+        mean = (double)sum / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 더블형이도 인트형으로 저장된다
+        //삼항연산자
+        grd = (mean >= 90) ? '수':
+              (mean >= 80) ? '우':
+              (mean >= 70) ? '미':
+              (mean >= 60) ? '양': '가';
 
-        /*System.out.println("이름을 입력하세요");
-        Scanner scanner = new Scanner(Syste.in);
-        name = scanner.nextLine();
-
-        System.out.println("국어 점수를 입력하세요");
-        kor = scanner.nextInt();
-
-        System.out.println("영어 점수를 입력하세요");
-        eng = scanner.nextInt();
-
-        System.out.println("수학 점수를 입력하세요");
-        mat = scanner.nextInt();*/
-
-        sum = kor + eng + mat;
-        mean = (kor+eng+mat)/3;
+        //결과 출력
         System.out.println("이름 : " + name);
         System.out.println("국어 : " + kor);
         System.out.println("영어 : " + eng);
         System.out.println("수학 : " + mat);
-
-        System.out.println("-----------");
-
-        System.out.println("총점 :" + sum);
-        System.out.println("평균 :" + mean);
-
-        if (mean >= 90 || mean <= 100) {
-            System.out.println("학점은" + grd1 + "입니다");
-        } else if (mean >= 80) {
-            System.out.println("학점은" + grd2 + "입니다");
-        } else if (mean >= 70) {
-            System.out.println("학점은" + grd3 + "입니다");
-        } else if (mean >= 60) {
-            System.out.println("학점은" + grd4 + "입니다");
-        } else System.out.println("학점은" + grd5 + "입니다");
+        System.out.println("--------------");
+        System.out.println("총점 : " + sum);
+        System.out.format("평균 : %2f \n",mean); //소수점 2자릿수까지만 표현하고 싶기 때문에
+        System.out.println("학점 : " + grd);
     }
 }
