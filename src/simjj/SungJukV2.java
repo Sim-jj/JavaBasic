8f20319a5e6b925fe65cc3a51b42098e42c6a0ea
Sim-jj <vnzkthsu@gmail.com>
2020-11-17 15:52:11 +0900

    선생님이 만든 성적 v2b

--- src/simjj/SungJukV2.java
+++ src/simjj/SungJukV2.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * 단,평균은 소수점 첫째 자리까지 출력하고
  * 학점기준은 수우미양가로 하되, switch문으로 학점을 계산하도록 함
  * 만점은 100점으로, 10점단위로 학점을 평가
- *  * 100~90,89~80,79~70,69~60,59~50
+ * 100~90,89~80,79~70,69~60,59~50
  * name,kor,eng,mat,sum,mean(평균),grd(학점)
  *
  *단, 성적처리 결과 출력시 출력문을 하나만 사용하기로 함
@@ -29,6 +29,9 @@ public class SungJukV2 {
         int sum = 0;
         double mean = 0.0;
         char grd = '가';
+        String fmt =
+                "이름 : %s\n국어: %d\n영어: %d\n수학: %d\n총점: %d\n평균: %s\n학점: %c\n";
+        String result;
 
         /*처리
         성적데이터를 키보드로 입력 받기위해
@@ -51,7 +54,6 @@ public class SungJukV2 {
         sum =  kor + eng + mat;
         mean = (double)sum / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 double형이도 int형으로 저장된다
 
-        //학점 계산은 switch문으로 처리
         switch((int)(mean/10)) { //평균을 10으로 나누었을때 10,9,8...으로 떨어지기때문에(int형은 소수점이 생길경우 다 버린다/예를들어 9.8일경우 9)
             // ,mean이 실수형이기 때문에 int으로 캐스트하여 계산을 해준다
             case 10 :  //간단한 경우 는 왠만하면 한줄로 코딩하는게 좋다 10,9는 같은 처리를 하기 때문에 9: 부분에만 처리값을 넣어줌
@@ -62,20 +64,28 @@ public class SungJukV2 {
             default: grd = '가';
         }
 
-        /* //String.format(형식지정자, 변수들)
-        mean = Double.parseDouble(String.format("%.1f",mean));//String포맷은 문자열이기 때문에 실수형으로 변형시켜줘야 됨으로
-        //Double.parseDouble를 이용
-        */
-
-        mean = Math.round(mean*100)/100.0;
+        // Math.round(값) : 반올림 하는 메서드
+        // 98765.4321 => round(반올림) => 98765
+        // 98765.4321 * 100 => 9876543.21
+        // round(9876543.21) => 9876543
+        // 9876543 / 100 => 98765.43
+        mean = Math.round(mean*100)/100.0; //나눌때 100.0으로해야지 실수형으로 출력된다 (100으로 하면 정수형으로만출력)
+        
+        //String.valueOf(값)->숫자를 문자로변환
+        result = String.format(fmt, name,kor,eng,mat,sum,String.valueOf(mean),grd); //fmt 에 지정한 형식에대한 변수를 대입하여 값을 매치시킴
 
         //결과 출력
-        System.out.printf("이름 : %s \n국어 : %d \n영어 : %d \n수학 : %d \n",name,kor,eng,mat);
-        System.out.println("--------------");
-        System.out.printf("총점 : %d \n평균 : %f \n학점 : %c \n",sum,mean,grd);
-        //System.out.format("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
-        //System.out.printf("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
+       /*
+        System.out.printf("평균 : %.1f \n",mean); //소수점 1자릿수까지 표현
         System.out.println("평균 : " + mean);
         System.out.println("학점 : " + grd);
+        System.out.println("이름 : " + name + "\n" +
+                           "국어 : " + kor + "\n" +
+                           "영어 : " + eng + "\n" +
+                           "수학 : " + mat + "\n"); */
+        //문자열 연결 연산자(+)로 문장을 만드는 경우
+        //String 변수의 특성 때문에 성능 저하가 발생하게 된다
+
+        System.out.println(result);
     }
 }
