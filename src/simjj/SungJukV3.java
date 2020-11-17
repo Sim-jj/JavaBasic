a346087d0929fa31022420b45589af98aa554c0c
Sim-jj <vnzkthsu@gmail.com>
2020-11-17 17:09:56 +0900

    선생님이만든 완벽한 성적처리 v3

--- src/simjj/SungJukV3.java
+++ src/simjj/SungJukV3.java
@@ -21,24 +21,22 @@ import java.util.Scanner;
  */
 public class SungJukV3 {
     public static void main(String[] args) {
-        // 변수선언
-        String[] name = new String[3];
-        int[] kor = new int[3];
-        int[] eng = new int[3];
-        int[] mat = new int[3];
-        int[] sum = new int[3];
-        double[] mean = new double[3];
+        // 배열 변수선언
+        //자료형 변수명[] = new 자료형[크기]
+        String name[] = new String[3];
+        int kor[] = new int[3];
+        int eng[] = new int[3];
+        int mat[] = new int[3];
+        int sum[] = new int[3];
+        double mean[] = new double[3];
         char grd[] = new char[3];
-        int num = 0;
+        String fmt =
+                "이름 : %s\n국어: %d\n영어: %d\n수학: %d\n총점: %d\n평균: %.2f\n학점: %c\n";
 
-        /*처리
-        성적데이터를 키보드로 입력 받기위해
-        Scanner 클래스 초기화*/
-
+        //처리
         for(int i=0; i<3; i++) {
                 Scanner sc = new Scanner(System.in);
-                num = num + 1;
-                System.out.println(num + "번째 학생 성적 입력중..");
+                System.out.println((i+1) + "번째 학생 성적 입력중..");
 
                 System.out.print("이름을 입력하세요 : "); //print->는 줄바꿈을 안한다
                 name[i] = sc.nextLine(); //nextLine();-> 문자열 입력 받을때
@@ -51,21 +49,14 @@ public class SungJukV3 {
 
                 System.out.print("수학 점수를 입력하세요 : ");
                 mat[i] = sc.nextInt();
-
-                sum[i] = kor[i] + eng[i] + mat[i];
-                mean[i] = (double) sum[i] / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 double형이도 int형으로 저장된다
-
             }
 
         //처리
-       /* for(int i=0; i<sum.length; i++) {
+        for(int i=0; i<mean.length;i++){
             sum[i] = kor[i] + eng[i] + mat[i];
             mean[i] = (double) sum[i] / 3; // (kor + eng + mat)/3 해도댐 ,정수로 나누면 각납처가 double형이도 int형으로 저장된다
 
-        }*/
-        for(int i=0; i<mean.length;i++){
-
-        switch((int)(mean[i]/10)) { //평균을 10으로 나누었을때 10,9,8...으로 떨어지기때문에(int형은 소수점이 생길경우 다 버린다/예를들어 9.8일경우 9)
+            switch((int)(mean[i]/10)) { //평균을 10으로 나누었을때 10,9,8...으로 떨어지기때문에(int형은 소수점이 생길경우 다 버린다/예를들어 9.8일경우 9)
             // ,mean이 실수형이기 때문에 int으로 캐스트하여 계산을 해준다
             case 10 :  //간단한 경우 는 왠만하면 한줄로 코딩하는게 좋다 10,9는 같은 처리를 하기 때문에 9: 부분에만 처리값을 넣어줌
             case 9 : grd[i] = '수'; break;
@@ -73,25 +64,13 @@ public class SungJukV3 {
             case 7 : grd[i] = '미'; break;
             case 6 : grd[i] = '양'; break;
             default: grd[i] = '가';
-        }
+            }
         }
         //결과 출력
-        num = 1;
-
 
         for(int i=0;i<grd.length;i++){
-            num = num+1;
-            mean[i] = Double.parseDouble(String.format("%.1f",mean));
-            System.out.println(num + "번째 학생 성적 데이터 출력");
-            System.out.println("이름 : " + name[i]);
-            System.out.println("국어 : " + kor[i]);
-            System.out.println("영어 : " + eng[i]);
-            System.out.println("수학 : " + mat[i]);
-
-            System.out.println("-----------");
-
-            System.out.println("총점 :" + sum[i]);
-            System.out.println("평균 :" + mean[i]);
+            System.out.println((i+1) + "번째 학생 성적 데이터 출력");
+            System.out.printf(fmt,name[i],kor[i],eng[i],mat[i],sum[i],mean[i],grd[i]);
         }
     }
 }
